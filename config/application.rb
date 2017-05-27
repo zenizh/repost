bb6990132a2ff363c1cc40cfac4ea7e825f47cbe
kami-zh <h.zenigami@everyleaf.com>
2017-05-27 20:15:01 +0900

    Change application name

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Shirase
+module Repoem
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
