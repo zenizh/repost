5d352c589dfa4f433ed57ccaae64210c78bb27b1
kami-zh <h.zenigami@everyleaf.com>
2017-06-04 13:50:54 +0900

    Refactor store

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { compose, createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import createHistory from 'history/createBrowserHistory'
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
-import persistState from 'redux-localstorage'
+import { ConnectedRouter } from 'react-router-redux'
+import { history, store } from './store'
 import './styles/application.global.scss'
 import Routes from './routes'
-import rootReducer from './reducers'
-
-const history = createHistory()
-const routeMiddleware = routerMiddleware(history)
-const middleware = applyMiddleware(routeMiddleware, thunk)
-const enhancer = compose(middleware, persistState('currentUser'))
-const store = createStore(rootReducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ app/javascript/packs/store.js
@@ -0,0 +1,14 @@
+import { compose, createStore, applyMiddleware } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import thunk from 'redux-thunk'
+import persistState from 'redux-localstorage'
+import rootReducer from './reducers'
+
+const history = createHistory()
+const routeMiddleware = routerMiddleware(history)
+const middleware = applyMiddleware(routeMiddleware, thunk)
+const enhancer = compose(middleware, persistState('currentUser'))
+const store = createStore(rootReducer, enhancer)
+
+export { history, store }
