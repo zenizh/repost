c5ad66ee669a3a710035e710e01c111d18215005
kami-zh <h.zenigami@everyleaf.com>
2017-06-04 13:38:06 +0900

    Refactor router

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom'
 import { compose, createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createHistory from 'history/createBrowserHistory'
-import { Route } from 'react-router'
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import persistState from 'redux-localstorage'
-
-import rootReducer from './reducers'
 import './styles/application.global.scss'
-import Header from './containers/Header'
-import Home from './containers/Home'
-import SignUp from './containers/SignUp'
-import SignIn from './containers/SignIn'
-import SignOut from './containers/SignOut'
-import Account from './containers/Account'
-import Channel from './containers/Channel'
-import { Authenticated, NotAuthenticated } from './lib/auth'
+import Routes from './routes'
+import rootReducer from './reducers'
 
 const history = createHistory()
 const routeMiddleware = routerMiddleware(history)
@@ -28,15 +19,7 @@ const store = createStore(rootReducer, enhancer)
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <Header />
-        <Route path="/sign_up" component={NotAuthenticated(SignUp)} />
-        <Route path="/sign_in" component={NotAuthenticated(SignIn)} />
-        <Route exact path="/" component={Authenticated(Home)} />
-        <Route path="/sign_out" component={Authenticated(SignOut)} />
-        <Route path="/account" component={Authenticated(Account)} />
-        <Route path="/channels/:id" component={Authenticated(Channel)} />
-      </div>
+      <Routes />
     </ConnectedRouter>
   </Provider>,
   document.body.appendChild(document.createElement('div'))
---
+++ app/javascript/packs/routes.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router'
+import Header from './containers/Header'
+import Home from './containers/Home'
+import SignUp from './containers/SignUp'
+import SignIn from './containers/SignIn'
+import SignOut from './containers/SignOut'
+import Account from './containers/Account'
+import Channel from './containers/Channel'
+import { Authenticated, NotAuthenticated } from './lib/auth'
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Route path="/sign_up" component={NotAuthenticated(SignUp)} />
+        <Route path="/sign_in" component={NotAuthenticated(SignIn)} />
+        <Route exact path="/" component={Authenticated(Home)} />
+        <Route path="/sign_out" component={Authenticated(SignOut)} />
+        <Route path="/account" component={Authenticated(Account)} />
+        <Route path="/channels/:id" component={Authenticated(Channel)} />
+      </div>
+    )
+  }
+}
+
+export default Routes
