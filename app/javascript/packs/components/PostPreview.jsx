df8093dbaf9299d78ee03ce778e6eec3214b885c
kami-zh <h.zenigami@everyleaf.com>
2017-06-15 17:57:54 +0900

    Use remark instead of react-remarkable

--- app/javascript/packs/components/PostPreview.jsx
+++ app/javascript/packs/components/PostPreview.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import remark from 'remark'
+import reactRenderer from 'remark-react'
 import CSSModules from 'react-css-modules'
 import styles from '../styles/PostPreview.scss'
-
-// TODO: Replace markdown renderer with remark when it adapts to ES6
-// See https://github.com/wooorm/remark/pull/262
-import Markdown from 'react-remarkable'
 
 const PostPreview = (props) => {
   const content = props.editor.state.getCurrentContent().getPlainText()
   return (
     <div styleName="container">
-      <Markdown source={content} />
+      {remark().use(reactRenderer).processSync(content).contents}
     </div>
   )
 }
---
+++ config/webpack/loaders/json.js
@@ -0,0 +1,4 @@
+module.exports = {
+  test: /\.json$/,
+  use: 'json-loader'
+}
