07e5060e997933212f647dd0a4b6ab0a38dc6f59
kami-zh <h.zenigami@everyleaf.com>
2017-06-19 12:16:19 +0900

    Change migration

--- db/migrate/20170608011009_create_teams.rb
+++ db/migrate/20170608011009_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.1]
   def change
     create_table :teams do |t|
-      t.string :name
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 20170608011009) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
