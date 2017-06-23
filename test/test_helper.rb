b24f866fb4996b373312b48efb979b8a6999cb6b
kami-zh <h.zenigami@everyleaf.com>
2017-06-24 01:56:55 +0900

    Allow request to localhost api

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'webmock/minitest'
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
