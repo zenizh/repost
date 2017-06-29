6af4c2ddcf7e5c7c43574f0282bad8bc8ea0c86a
kami-zh <h.zenigami@everyleaf.com>
2017-06-29 10:26:04 +0900

    Move JavaScript files to out of packs directory
    See https://github.com/rails/webpacker/issues/444#issuecomment-311729732

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import './styles/application.global.scss'
-import Routes from './config/routes'
-import { history, store } from './config/store'
+import '../styles/application.global.scss'
+import Routes from '../config/routes'
+import { history, store } from '../config/store'
 
 ReactDOM.render(
   <Provider store={store}>
