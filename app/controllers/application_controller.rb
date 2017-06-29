2264adc21a586e739560aa31c0a71fe7ed65da2a
kami-zh <h.zenigami@everyleaf.com>
2017-06-29 11:03:12 +0900

    Authenticate with Basic if username and password are set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_with_basic
+
   def root
     render html: '', layout: true
+  end
+
+  private
+
+  def authenticate_with_basic
+    if !ENV['BASIC_USERNAME'] || !ENV['BASIC_PASSWORD']
+      return
+    end
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
+    end
   end
 end
