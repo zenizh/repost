141568a8e958b485d763bdcac599ba33ba4692b4
kami-zh <h.zenigami@everyleaf.com>
2017-06-30 15:42:18 +0900

    Show tooltip on mouse on user image

--- app/javascript/components/UserList.jsx
+++ app/javascript/components/UserList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { UncontrolledTooltip } from 'reactstrap'
 import CSSModules from 'react-css-modules'
 import styles from '../styles/UserList.scss'
 
@@ -13,7 +14,15 @@ const UserList = (props) => {
       <h3>Users</h3>
       <ul>
         {props.users.map((user, key) => {
-          return <li key={key}><img src={user.avatar} /></li>
+          const id = `user_${user.id}`
+          return (
+            <li key={key} id={id}>
+              <img src={user.avatar} />
+              <UncontrolledTooltip placement="bottom" target={id}>
+                {user.name}
+              </UncontrolledTooltip>
+            </li>
+          )
         })}
       </ul>
     </div>
