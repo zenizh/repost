d08766b40df35e27ce3648f924e43fac4abefe9e
kami-zh <hiroki.zenigami@gmail.com>
2017-07-12 14:36:41 +0900

    Clear reactions on set post

--- app/javascript/reducers/reactions.js
+++ app/javascript/reducers/reactions.js
@@ -1,4 +1,5 @@
 import { CREATE_REACTION } from '../actions/reactionsActions'
+import { SET_POST } from '../actions/postActions'
 
 const initialState = []
 
@@ -6,6 +7,8 @@ function reactions(state = initialState, action) {
   switch (action.type) {
     case CREATE_REACTION + '_SUCCESS':
       return [...state, action.payload.data]
+    case SET_POST:
+      return initialState
     default:
       return state
   }
