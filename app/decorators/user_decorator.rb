10e49be41aacdffa3b7b8172f9d3e7272dae7616
kami-zh <hiroki.zenigami@gmail.com>
2018-01-08 22:00:04 +0900

    Improve var name

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,11 +3,11 @@ module UserDecorator
     name.presence || screen_name
   end
 
-  def role_value_for_change
+  def role_value_for_update
     admin? ? 'member' : 'admin'
   end
 
-  def role_label_for_change
+  def role_label_for_update
     admin? ? 'Remove Admin' : 'Make Admin'
   end
 end
