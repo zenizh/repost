511f122a75d9fdad5665ceb473e1ef734f5a89a6
kami-zh <hiroki.zenigami@gmail.com>
2018-01-16 07:16:12 +0900

    Refactor star button

---
+++ app/javascript/controllers/button_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  connect() {
+    this.render()
+  }
+
+  click(e) {
+    this.isPressed = !this.isPressed
+    this.render()
+  }
+
+  render() {
+    if (this.isPressed) {
+      this.press.style.display = 'none'
+      this.unpress.style.display = 'block'
+    } else {
+      this.press.style.display = 'block'
+      this.unpress.style.display = 'none'
+    }
+  }
+
+  get isPressed() {
+    return this.data.get('is-pressed') == 'true'
+  }
+
+  get press() {
+    return this.targets.find('press')
+  }
+
+  get unpress() {
+    return this.targets.find('unpress')
+  }
+
+  set isPressed(isPressed) {
+    this.data.set('is-pressed', isPressed)
+  }
+}
