47982a866aedad3aff55f671a4461e26b16a2ff8
kami-zh <hiroki.zenigami@gmail.com>
2018-01-21 20:05:48 +0900

    nits

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
-      .eager_load(comment: [:post, { user: :avatar_attachment } ])
+      .eager_load(comment: [:post, { user: :avatar_attachment }])
       .order(created_at: :desc)
       .page(params[:page])
     @notifications.unread.update_all(status: :read)
