49be9ab603da0db08805f737d821d4d526577b5e
kami-zh <hiroki.zenigami@gmail.com>
2018-02-06 18:37:30 +0900

    Refactor like and star feature

---
+++ app/javascript/controllers/like_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['active', 'inactive', 'count']
+
+  initialize() {
+    this.render()
+  }
+
+  click() {
+    this.isLiked ? this.count-- : this.count++
+    this.isLiked = !this.isLiked
+    this.render()
+  }
+
+  render() {
+    this.isLiked ? this.showActive() : this.showInactive()
+    this.countTarget.innerText = this.count
+  }
+
+  showActive() {
+    this.activeTarget.style.display = 'inline'
+    this.inactiveTarget.style.display = 'none'
+  }
+
+  showInactive() {
+    this.activeTarget.style.display = 'none'
+    this.inactiveTarget.style.display = 'inline'
+  }
+
+  get count() {
+    return parseInt(this.data.get('count'))
+  }
+
+  get isLiked() {
+    return this.data.get('isLiked') == 'true'
+  }
+
+  set count(count) {
+    this.data.set('count', count)
+  }
+
+  set isLiked(isLiked) {
+    this.data.set('isLiked', isLiked)
+  }
+}
---
+++ app/javascript/controllers/star_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['active', 'inactive']
+
+  initialize() {
+    this.render()
+  }
+
+  click() {
+    this.isStarred = !this.isStarred
+    this.render()
+  }
+
+  render() {
+    this.isStarred ? this.showActive() : this.showInactive()
+  }
+
+  showActive() {
+    this.activeTarget.style.display = 'inline'
+    this.inactiveTarget.style.display = 'none'
+  }
+
+  showInactive() {
+    this.activeTarget.style.display = 'none'
+    this.inactiveTarget.style.display = 'inline'
+  }
+
+  get isStarred() {
+    return this.data.get('isStarred') == 'true'
+  }
+
+  set isStarred(isStarred) {
+    this.data.set('isStarred', isStarred)
+  }
+}
