693f9b3949dc7fc307f26b843bd38fb546e793d1
kami-zh <hiroki.zenigami@gmail.com>
2018-02-06 23:48:58 +0900

    Refactor search form

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,28 +1,44 @@
 module SearchHelper
-  def link_to_search_by(attribute, type, value = nil, &block)
+  def link_to_search_by_starred(&block)
+    link_to_search_by_boolean(:starred, &block)
+  end
+
+  def link_to_search_by_tag(tag, &block)
+    link_to_search_by_array(:tags, tag.name, &block)
+  end
+
+  def link_to_search_by_user(user, &block)
+    link_to_search_by_array(:users, user.screen_name, &block)
+  end
+
+  def link_to_search_by_boolean(attribute, &block)
     params = search_params
 
-    case type
-    when :array
-      params[attribute] ||= []
-
-      if value.in?(params[attribute])
-        params[attribute].delete(value)
-        active = true
-      else
-        active = true if params[attribute].empty?
-        params[attribute] << value
-      end
-    when :boolean
-      if params[attribute] == 'true'
-        params[attribute] = nil
-        active = true
-      else
-        params[attribute] = true
-      end
+    if params[attribute] == 'true'
+      active = true
+      params[attribute] = nil
+    else
+      params[attribute] = true
     end
 
-    link_to url_for(params), class: ['label-light small', ('active' if active)] do
+    link_to url_for(params), class: ('active' if active) do
+      block.call
+    end
+  end
+
+  def link_to_search_by_array(attribute, value, &block)
+    params = search_params
+    params[attribute] ||= []
+
+    if value.in?(params[attribute])
+      active = true
+      params[attribute].delete(value)
+    else
+      active = true if params[attribute].empty?
+      params[attribute] << value
+    end
+
+    link_to url_for(params), class: ('active' if active) do
       block.call
     end
   end
