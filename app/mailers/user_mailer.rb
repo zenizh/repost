0b1731a70b1d97ff4dd137aefd553d8965e4e34a
kami-zh <hiroki.zenigami@gmail.com>
2018-02-07 23:29:22 +0900

    Set subject to emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,18 +2,18 @@ class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
     @url = new_users_activation_url(token: @user.activation_token)
-    mail(to: user.email)
+    mail(to: user.email, subject: '[Repost] Welcome to Repost')
   end
 
   def activation_success_email(user)
     @user = user
     @url = sign_in_url
-    mail(to: user.email)
+    mail(to: user.email, subject: '[Repost] Your account has been activated')
   end
 
   def reset_password_email(user)
     @user = user
     @url = edit_users_password_url(token: user.reset_password_token)
-    mail(to: user.email)
+    mail(to: user.email, subject: '[Repost] Your password has been reset')
   end
 end
